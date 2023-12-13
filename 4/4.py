df1f1b35b72240967718dbda9ad741a3c44ae979
Matt Jackson <m.jackson@tarabutgateway.com>
2023-12-13 18:56:06 +0000

    day 4 pt 2

--- 4/4.py
+++ 4/4.py
@@ -1,10 +1,22 @@
 TEST_INPUT = 'test.txt'
 REAL_INPUT = 'cards.txt'
 
+def get_number_of_matches(card, winners):
+    return sum(card_number in winners for card_number in card)
 
 def get_point_value(card, winners):
-    num_winners = sum(card_number in winners for card_number in card)
+    num_winners = get_number_of_matches(card, winners) 
     return 2**(num_winners-1) if num_winners else 0
+
+def get_number_of_cards(winners_and_cards):  # part 2
+    number_of_cards = {card_num: 1 for card_num in range(1, len(winners_and_cards)+1)}
+    for card_num, (winners, card) in enumerate(winners_and_cards, start=1):
+        number_of_matches_for_card = get_number_of_matches(card, winners)
+        for card_copy in range(number_of_cards[card_num]):
+            for win in range(1, number_of_matches_for_card+1):
+                number_of_cards[card_num+win] += 1
+
+    return sum(number_of_cards.values())
 
 def import_draw_and_winners(input_line):
     draw = []
@@ -26,7 +38,7 @@ def import_from_file(filename):
 
 def main():
     winners_and_cards = import_from_file(REAL_INPUT)
-    return sum([get_point_value(cards, winners) for winners, cards in winners_and_cards])
+    return get_number_of_cards(winners_and_cards)
 
 if __name__=='__main__':
     print(main())
